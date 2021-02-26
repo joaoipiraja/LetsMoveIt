3560b112fa957742df2c10f3d8de0ff6c4c93dc7
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-02-25 21:47:43 -0300

    Add challengeCompleted of Context

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompletedChallenges() {
+    const { challengeCompleted } = useContext(challengesContext);
+
     return (
         <div className={styles.completedChallengesContainer}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{challengeCompleted}</span>
         </div>
     );
 }
