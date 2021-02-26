d61bae626aa2c9e0eab3523c837b6ab31aaf291d
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-02-26 08:34:02 -0300

    Add level from challengesContext to Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,10 @@
 
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(challengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/joaoipiraja.png" alt="" />
@@ -9,7 +12,7 @@ export function Profile() {
                 <strong> João Victor Ipirajá</strong>
                 <p>
                     <img src="icons/level.svg" alt="" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
