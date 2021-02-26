3aa50ffc4899f3d5b7ed29f5f205e7b51279aeb6
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-02-26 13:54:51 -0300

    Add GetServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,30 +3,55 @@ import { CountDown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import Head from 'next/head';
-
+import { GetServerSideProps } from 'next';
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CountDownProvider } from '../contexts/CountDownContext';
+import { ChallengeProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <CountDownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <CountDown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountDownProvider>
-    </div>
+    <ChallengeProvider level={props.level} currentExperience={props.currentExperience} challengesCompleted={props.challengesCompleted}>
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move.it</title>
+        </Head>
+        <ExperienceBar />
+        <CountDownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <CountDown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountDownProvider>
+      </div>
+    </ChallengeProvider>
   )
+}
+
+//Onde o Next Surgiiu
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  //chamada API
+  const { level, currentExperience, challengeCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengeCompleted: Number(challengeCompleted)
+    }
+  }
 }
