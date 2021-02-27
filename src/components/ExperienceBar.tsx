43f74cba1605bf426cc675fd3ebeaf6f8be7d1ee
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-02-27 10:55:54 -0300

    Update to animated ExperienceBar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,18 +1,41 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
+import LinearProgress, { LinearProgressProps } from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import { challengesContext } from '../contexts/ChallengesContext';
-import styles from '../styles/components/ExperienceBar.module.css'
+
+
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+        <Box display="flex" alignItems="center">
+            <Box width="100%" mr={1}>
+                <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box minWidth={35}>
+                <Typography variant="body2" color="textSecondary">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+});
+
 
 export function ExperienceBar() {
+    const classes = useStyles();
     const { currentExperience, experienceToNextLevel } = useContext(challengesContext)
     const percentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel);
+
     return (
-        <header className={styles.experienceBar}>
-            <span>0 xp</span>
-            <div>
-                <div style={{ width: `${percentToNextLevel}%` }} />
-                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>{currentExperience} xp</span>
-            </div>
-            <span>{experienceToNextLevel}xp</span>
-        </header>
+        <div className={classes.root}>
+            <LinearProgressWithLabel value={percentToNextLevel} />
+        </div>
     );
 }
